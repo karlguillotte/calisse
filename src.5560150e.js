740804f95cc9549993c53d724affab47ef56404e
karlguillotte <karl.guillotte.1@gmail.com>
2019-04-27 15:41:45 -0700

    Updates

--- src.5560150e.js
+++ src.5560150e.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./styles.css");var e=t(require("./map"));function t(e){return e&&e.__esModule?e:{default:e}}(0,e.default)(),document.addEventListener("scroll",function(){document.getElementById("top").style.display=(document.body.scrollTop||document.documentElement.scrollTop)>window.innerHeight/2?"block":"none"});
 },{"./styles.css":"Tnu0","./map":"qu/t"}]},{},["Focm"], null)
-//# sourceMappingURL=/calisse/src.ac56afe1.js.map
+//# sourceMappingURL=/calisse/src.5560150e.js.map
