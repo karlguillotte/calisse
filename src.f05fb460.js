42770b9a0176ec09fe76a1a03dd794619ceb3a98
karlguillotte <karl.guillotte.1@gmail.com>
2019-04-27 14:50:41 -0700

    Updates

---
+++ src.f05fb460.js
@@ -0,0 +1,8 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"Tnu0":[function(require,module,exports) {
+
+},{}],"qu/t":[function(require,module,exports) {
+"use strict";function e(){var e=new mapboxgl.Map({container:"map",style:"mapbox://styles/mapbox/outdoors-v9",center:[-118.2,51],zoom:9});return e.scrollZoom.disable(),e}Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=e,mapboxgl.accessToken="pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA";
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("./styles.css");var e=r(require("./map"));function r(e){return e&&e.__esModule?e:{default:e}}(0,e.default)();
+},{"./styles.css":"Tnu0","./map":"qu/t"}]},{},["Focm"], null)
+//# sourceMappingURL=/src.f05fb460.js.map
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import './styles.css'
-import init from './map'
-
-init()
