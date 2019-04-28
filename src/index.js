8763c518257e207432fbff6d0f6c1ca37544079d
karlguillotte <karl.guillotte.1@gmail.com>
2019-04-27 22:13:23 -0700

    Latest

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import './styles.css'
-import init from './map'
+import initMap from './map'
 
-init()
+initMap()
+
+document.addEventListener('scroll', handleScroll)
+handleScroll()
+
+function handleScroll() {
+    document.getElementById('top').style.display =
+        (document.body.scrollTop || document.documentElement.scrollTop) >
+        window.innerHeight / 2
+            ? 'block'
+            : 'none'
+}
--- src/map.js
+++ src/map.js
@@ -1,16 +1,41 @@
+import data from './segments.json'
+
 mapboxgl.accessToken =
-    'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA'
+    'pk.eyJ1Ijoia2d1aWxsb3R0ZSIsImEiOiJjaW95czF5bHMwMXprdWNtODhndnhpM2wwIn0.ySK8KKQ5AVs3FYQ9fbQJVg'
 
 export default function init() {
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/outdoors-v9',
-        // style: 'mapbox://styles/mapbox/satellite-streets-v9',
-        center: [-118.2, 51],
-        zoom: 9,
+        center: [-118.23, 51],
+        zoom: 11,
     })
-    
+
     map.scrollZoom.disable()
+    map.addControl(new mapboxgl.NavigationControl())
+
+    map.on('moveend', () => {
+        console.warn(map.getCenter())
+    })
+
+    map.on('load', () => {
+        map.addLayer({
+            id: 'route',
+            type: 'line',
+            source: {
+                type: 'geojson',
+                data,
+            },
+            layout: {
+                'line-join': 'round',
+                'line-cap': 'round',
+            },
+            paint: {
+                'line-color': '#888',
+                'line-width': 8,
+            },
+        })
+    })
 
     return map
 }
